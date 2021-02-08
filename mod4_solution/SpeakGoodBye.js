cab14f3e34bbcc250ec1c5b0e2dbdc5112661b8a
潘冬平 <pan@hanfuyutairas-MacBook-Pro.local>
2021-02-08 20:31:47 +0900

    assignment4

--- mod4_solution/SpeakGoodBye.js
+++ mod4_solution/SpeakGoodBye.js
@@ -1,4 +1,4 @@
-// NOTE! The steps in this file are basically identical to the ones you
+( function(window){// NOTE! The steps in this file are basically identical to the ones you
 // performed in the SpeakHello.js file.
 
 // STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
@@ -12,13 +12,15 @@
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
 var speakWord = "Good Bye";
-
+byeSpeaker ={};
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
+byeSpeaker.speak = function(name){
   console.log(speakWord + " " + name);
 }
+window.byeSpeaker = byeSpeaker;
+})(window);
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
--- mod4_solution/script.js
+++ mod4_solution/script.js
@@ -34,14 +34,17 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+(function(window){
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
+for (var name in names) {
+
+  var first_low_case = names[name].charAt(0).toLowerCase()
 
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
@@ -57,9 +60,10 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
+  if (first_low_case === "j") {
+    byeSpeaker.speak(names[name])
   } else {
-    // helloSpeaker.xxxx
+    helloSpeaker.speak(names[name])
   }
-}
+}}
+)(window);
